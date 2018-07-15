8b859547b2a63d2e903d728d6ce1edc6fb52fa40
qufulin <qufulin@55haitao.com>
2018-07-15 23:25:06 +0800

    init

---
+++ src/Func/Arr.php
@@ -0,0 +1,30 @@
+<?
+namespace Lexin\Func;
+
+class Arr
+{
+    //获取数组内容
+    public function array_get($array, $key, $default = null) {
+        if (!is_array($array)) {
+            return $default;
+        }
+
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (array_key_exists($key, $array)) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (is_array($array) && array_key_exists($segment, $array)) {
+                $array = $array[$segment];
+            } else {
+                return $default;
+            }
+        }
+
+        return $array;
+    }
+}
